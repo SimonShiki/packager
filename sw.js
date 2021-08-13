a3503181f920a534ca8b70af32b80f2ee77df5a9
GarboMuffin <GarboMuffin@users.noreply.github.com>
2021-08-13 02:40:05 +0000

    Deploying to gh-pages from @ TurboWarp/packager@ac98fc5083741e980980273ea8dcb21144bbb3ef 🚀

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // These will be replaced at build-time by generate-service-worker-plugin.js
-const ASSETS = ["/","checksum.worker.js","downloader.worker.js","assets/default-icon.9d63d3cfa69f175efb0a137a4a1e7c09.png","js/vendors~icns~jszip.0e98a91245bc77c62d9b.js","js/icns.cfb418c6e27b5c0e8d2f.js","js/jszip.898f71d88acbcb6793b6.js","js/packager.02bc009d1a785e33e0a2.js","js/vendors~icns.6a3ecab32310c289056d.js","js/vendors~jszip.e36b422aacd71c799df5.js","js/vendors~packager.992889b6cf90a1a2bd0f.js"];
-const CACHE_NAME = 'p4-f4003f94ff407c6a39028f5973f3d1df6af592c2180739788f2830fb228cd20d';
+const ASSETS = ["/","checksum.worker.js","downloader.worker.js","assets/default-icon.9d63d3cfa69f175efb0a137a4a1e7c09.png","js/vendors~icns~jszip.0e98a91245bc77c62d9b.js","js/icns.cfb418c6e27b5c0e8d2f.js","js/jszip.898f71d88acbcb6793b6.js","js/packager.928968fc480599dde9b9.js","js/vendors~icns.6a3ecab32310c289056d.js","js/vendors~jszip.e36b422aacd71c799df5.js","js/vendors~packager.992889b6cf90a1a2bd0f.js"];
+const CACHE_NAME = 'p4-da56c0d3c60dec08bef9a0b0cd9d90a36d035d9e422ddea5887b866cafdb3839';
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
