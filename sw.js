e12ff9be5dd59e9700224b05e25b1380c991034f
Thomas Weber <weber_t@mailbox.org>
2021-08-21 13:05:31 -0500

    Revert "Deploying to gh-pages from @ TurboWarp/packager@9114dc8c4c47150103b5af9e3b70d4b589bf4984 🚀"
    This reverts commit bc48afca1fc00aa5f09b319567c443dd4b6a406e.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // These will be replaced at build-time by generate-service-worker-plugin.js
-const ASSETS = ["/","checksum.worker.js","downloader.worker.js","assets/default-icon.9d63d3cfa69f175efb0a137a4a1e7c09.png","js/vendors~icns~jszip.faa09448de34f4d4a305.js","js/icns.e487eba4a2ec53ac2889.js","js/jszip.adb1f58076fbe3ba2201.js","js/packager.690a71e1b6a503ec5df0.js","js/vendors~icns.2f6b87d320656fdd2841.js","js/vendors~jszip.aea786587ab9cd066968.js","js/vendors~packager.15d689f3e35b15ae1a15.js"];
-const CACHE_NAME = 'p4-03e52e1cd9b4a3e7bc3a74c215014ca9f6482509830c775e8e99ec56cf3152bc';
+const ASSETS = ["/","checksum.worker.js","downloader.worker.js","assets/default-icon.9d63d3cfa69f175efb0a137a4a1e7c09.png","js/vendors~icns~jszip.b20b77213f5d33512981.js","js/icns.a29318a7264e67faf326.js","js/jszip.859aa117691d50f1c0d9.js","js/packager.1e81615538e031846272.js","js/vendors~icns.a0ae71b2e9ba3cdf6fc2.js","js/vendors~jszip.7a5694a0d807f5cdcedd.js","js/vendors~packager.d5eed487ecd042996762.js"];
+const CACHE_NAME = 'p4-e0efa10fd464b70409434e3e531bf93df6f448cedfb457821f44dc1f648a149a';
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
