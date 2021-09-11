366297837baf77d5b8943c45eac2e0b0c17efb8a
GarboMuffin <GarboMuffin@users.noreply.github.com>
2021-09-11 22:19:17 +0000

    Deploying to gh-pages from @ TurboWarp/packager@fc45edbfa4f54ba02d36ce9506b805e718eb7adf 🚀

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // These will be replaced at build-time by generate-service-worker-plugin.js
-const ASSETS = ["","js/sha256.52086ae4fb19cece366a.worker.js","js/download-project.84c8a5c9587020de48cd.worker.js","assets/default-icon.290e09e569a1cab8e61ba93b0d23863f.png","js/vendors~icns~jszip.5eb2f78365755579798f.js","js/icns.e07b1610890b2ebcdc3a.js","js/jszip.50c3dd8a94e2b060c0b3.js","js/packager.229a5d8c7cfcc69a7e88.js","js/packager-options-ui.de48efbfa74f09fbc538.js"];
-const CACHE_NAME = 'p4-d66e3dd255d798629665390936ce66e48da0d00b9ede024aba3a5e8efbe3ea25';
-const IS_PRODUCTION = 'production' === 'production';
+const ASSETS = ["","js/sha256.52086ae4fb19cece366a.worker.js","js/download-project.84c8a5c9587020de48cd.worker.js","assets/default-icon.290e09e569a1cab8e61ba93b0d23863f.png","js/vendors~icns~jszip.5eb2f78365755579798f.js","js/icns.e07b1610890b2ebcdc3a.js","js/jszip.50c3dd8a94e2b060c0b3.js","js/packager.f5fa8f5fd7b4e66263dc.js","js/packager-options-ui.d86f2213ee2ba6dba051.js"];
+const CACHE_NAME = "p4-2d7c5259e7cf57db580364c8e334c9d272ebc8b910e3a7e5ec766160b9d3fa4f";
+const IS_PRODUCTION = true;
 
 const base = location.pathname.substr(0, location.pathname.indexOf('sw.js'));
 
